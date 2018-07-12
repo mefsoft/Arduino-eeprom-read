f9b077e6350f146e6628acd1f9f2cdb220301c93
muhammet furkan çiftci <mfc_923@hotmail.com>
2018-07-12 22:47:34 +0300

    Add files via upload

---
+++ eeprom_okuma.ino
@@ -0,0 +1,20 @@
+/*Powered By MEFSOFT*/
+/*You can use you can use this program, but the publishing rights are reserved by MEFSOFT*/
+/*Unauthorized publishing and sale of the program is prohibited.*/
+/* V-1.1*/
+/*You can change size number for yourself.*/
+#include <Wire.h>                          //Include the Wire.h library so we can communicate with the gyro.
+#include <EEPROM.h> 
+void setup() {
+int size=40; 
+Serial.begin(57600);
+for(int start = 0; start <= size; start++)
+{
+  Serial.println( EEPROM.read(start));
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
